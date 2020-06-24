14e3981c1cd941e13a0203d38afb8eac38cb884f
RissaSams <rissa.sams@yahoo.com>
2020-06-24 12:26:45 -0500

    updates

--- routes/test.js
+++ routes/test.js
@@ -7,6 +7,22 @@ router.get('/', function (req, res, next) {
     res.render('test', {});
 });
 
+router.post('/substring', function(req,res,next){
+    let text = req.body.text1;
+    let num5 = req.body.number5;
+    let num6 = req.body.number6;
+    let result;
+
+    result = text.substring(num5,num6);
+
+    res.render('test', {
+        text1: text,
+        number5: num5,
+        number6: num6,
+        substringResult: result
+    });
+})
+
 router.post('/find', function (req, res, next) {
     let text = req.body.text3;
     let searchWord = req.body.findWord;
@@ -46,5 +62,74 @@ router.post('/compare', function (req, res, next) {
         number3: num3,
         number4: num4,
         compareResult: result
+    });
+});
+
+function factorialize(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+  }
+  function fibonacci(n) {
+    return n < 1 ? 0
+         : n <= 2 ? 1
+         : fibonacci(n - 1) + fibonacci(n - 2);
+ }
+router.post('/math', function (req, res, next) {
+    let num1 = parseInt(req.body.number1);
+    let num2 = parseInt(req.body.number2);
+    let oper = req.body.operator;
+    let out = req.body.output;
+    let result;
+
+    switch (oper) {
+        case '+':
+            result = num1 + num2;            
+            break;
+        case '-':
+            result = num1 - num2;            
+            break;
+        case '/':
+            result = num1 / num2;            
+            break;
+        case '*':
+            result = num1 * num2;            
+            break;
+        case '%':
+            result = num1 % num2;            
+            break;
+        case '!':
+            result = factorialize(num1);            
+            break;
+        case 'f':
+            result = fibonacci(num1);            
+            break;
+        case '^':
+            result = num1 ** num2;            
+            break;    
+        default:
+            break;
+    }
+
+    switch (out) {
+        case 'x':
+            result = result.toString(16);
+            break;
+        case 'b':
+            result = result.toString(2);
+            break;
+    }
+
+    //  return findResult to the browser
+    res.render('test', {
+        number1: num1,
+        number2: num2,
+        operator: oper,
+        output: out,
+        mathResult: result
     });
 });
